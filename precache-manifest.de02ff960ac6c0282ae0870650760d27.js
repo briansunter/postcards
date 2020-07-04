e275104bc55ead375f07227e9a2caecd8c6b259c
briansunter <bsunter@gmail.com>
2020-07-04 05:54:45 +0000

    Deploying to gh-pages from  @ 89844d039766e204a0ea7790440d42d95f123bea 🚀

---
+++ precache-manifest.de02ff960ac6c0282ae0870650760d27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92acde76bbb856b997c4cfa773361d09",
+    "url": "/postcards/index.html"
+  },
+  {
+    "revision": "409564a3da1c712c2f93",
+    "url": "/postcards/static/css/main.814f1d2c.chunk.css"
+  },
+  {
+    "revision": "f40df105a7b0992294c6",
+    "url": "/postcards/static/js/2.d73f5999.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/postcards/static/js/2.d73f5999.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "409564a3da1c712c2f93",
+    "url": "/postcards/static/js/main.903fd99f.chunk.js"
+  },
+  {
+    "revision": "67a01aff88fb3958645a",
+    "url": "/postcards/static/js/runtime-main.655adb5d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/postcards/static/media/logo.5d5d9eef.svg"
+  }
+]);
